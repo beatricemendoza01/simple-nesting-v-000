6487d984b300f6c1ff9b896f7f87ea9fa83fbce3
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-20 03:46:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -63,10 +63,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto] = {
+     programmer_hash << {
         :known_for => "Ruby",
         :languages => ["LISP", "C"]
       }
@@ -130,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
